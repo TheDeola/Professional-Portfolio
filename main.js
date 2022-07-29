e82694fd329456923305f5bb44548a2c2a95f0c5
TheDeola <aadetunji012@gmail.com>
2022-07-29 23:23:25 +0200

    new site update

---
+++ main.js
@@ -0,0 +1,27 @@
+// const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// const colors = ["green", "rgba(180, 17, 200, 0.5)", "red", "rgba(133,122,200)", "pink", "#f15025"];
+// const btn = document.getElementById('btn');
+// // const bgColorText = document.querySelector('.bgcolor-text');
+// const color = document.querySelector('.color')
+
+// btn.addEventListener('click', function(){
+//   let hexColor='#';
+//     for(let i = 0; i<6; i++){
+//     hexColor+=hex[getRanNum()]
+//   }
+//   color.textContent = hexColor;
+//   document.body.style.backgroundColor = hexColor;
+// })
+
+
+// btn.addEventListener('click', function(){
+//   const randomNumber = getRanNum();
+//   console.log(randomNumber)
+//   document.body.style.backgroundColor=colors[randomNumber];
+//   color.textContent=colors[randomNumber];
+//   // document.style.bgColorText=backgroundColor[randomNumber];
+// }) 
+// const getRanNum = () => {
+//   return Math.floor(Math.random()*colors.length); 
+// }
