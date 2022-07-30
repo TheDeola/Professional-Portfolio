45830c559ad2a4d3a6ce4a9e513fa3c1a0dcf4c1
TheDeola <aadetunji012@gmail.com>
2022-07-30 14:05:51 +0200

    scroll feature commit

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-// const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+var boxes = document.querySelectorAll('.box');
 
-// const colors = ["green", "rgba(180, 17, 200, 0.5)", "red", "rgba(133,122,200)", "pink", "#f15025"];
-// const btn = document.getElementById('btn');
-// // const bgColorText = document.querySelector('.bgcolor-text');
-// const color = document.querySelector('.color')
+window.addEventListener('scroll', checkBoxes)
 
-// btn.addEventListener('click', function(){
-//   let hexColor='#';
-//     for(let i = 0; i<6; i++){
-//     hexColor+=hex[getRanNum()]
-//   }
-//   color.textContent = hexColor;
-//   document.body.style.backgroundColor = hexColor;
-// })
+checkBoxes()
 
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4;
 
-// btn.addEventListener('click', function(){
-//   const randomNumber = getRanNum();
-//   console.log(randomNumber)
-//   document.body.style.backgroundColor=colors[randomNumber];
-//   color.textContent=colors[randomNumber];
-//   // document.style.bgColorText=backgroundColor[randomNumber];
-// }) 
-// const getRanNum = () => {
-//   return Math.floor(Math.random()*colors.length); 
-// }
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
